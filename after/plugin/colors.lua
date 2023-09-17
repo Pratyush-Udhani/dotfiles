f16504ff2615943ec75e951891a3adcce987834a
Pratyush-Udhani <pratyush.udhani@gmail.com>
2023-09-17 21:06:11 +0530

    command maps

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,12 @@
-function ColorChange(color)
+function Start(color)
 	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd("command! W wa")
+    vim.cmd("command! ZZ wq")
+
 end
 
-ColorChange()
+Start()
